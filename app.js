2c9512a4db282d0898fba9c438c38df3ed57a8fb
Aman Mishra <amanm4345@gmail.com>
2024-05-22 14:25:03 +0530

    FEAT: Implemented Web socket (Socket.io) connection and make a simple real time chat box application.

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const app = express();
 const connectDB = require("./Config/database");
 app.use(express.json());
 const port = 3000;
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+
+//Socket.io server
+const {Server} = require("socket.io");
+
+//Create server instance
+const io = new Server(server);
+
+//Handle socket.io
+io.on("connection", (socket) =>{
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+  });
+});
+
+//Render HTML file
+app.use(express.static(path.resolve("./public")));
+
+//Routes actions for Socket.io
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+})
 
 //Routes actions for user
 const usersRouter = require("./Routes/users");
@@ -11,6 +35,6 @@ app.use("/users", usersRouter);
 // Connect to MongoDB
 connectDB();
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
